cf685eedbf589fd4310b34e31d7b6f2368a8c5b7
nathalymendez2 <20240188@colegioscj.edu.gt>
2025-05-15 23:37:41 -0600

    nueva actualizacion

--- hola.py
+++ hola.py
@@ -15,11 +15,11 @@ def longitudes(tipo, entrada, resultado):
     
     if tipo == "metro_kilometro":
         resultadoo = valor /1000
-        resultado.config(text=f"{valor} pulgadas = {resultado} km")
+        resultado.config(text=f"{valor} pulgadas = {resultadoo} km")
     
     elif tipo == "pulgada_metro":
         resultadoo = valor * 0.02
-        resultado.config(text=f"{valor} pulgadas = {resultado} metros")
+        resultado.config(text=f"{valor} pulgadas = {resultadoo} metros")
 
 def masa(tipo, entrada, resultado):
     valor = validar(entrada.get())
@@ -28,11 +28,11 @@ def masa(tipo, entrada, resultado):
     
     if tipo == "kilogramo_gramo":
         resultadoo = valor*1000
-        resultado.config(text=f"{valor} kg = {resultado} g")
+        resultado.config(text=f"{valor} kg = {resultadoo} g")
     
     elif tipo == "libra_kilogramo":
         resultadoo = valor*0.4
-        resultado.config(text=f"{valor} lb = {resultado} kg")
+        resultado.config(text=f"{valor} lb = {resultadoo} kg")
 
 def tiempo(tipo, entrada, resultado):
     valor = validar(entrada.get())
@@ -41,55 +41,55 @@ def tiempo(tipo, entrada, resultado):
     
     if tipo == "segundo_minuto":
         resultadoo = valor/60
-        resultado.config(text=f"{valor} segundos = {resultado} minutos")
+        resultado.config(text=f"{valor} segundos = {resultadoo} minutos")
 
     elif tipo == "hora_dia":
         resultadoo = valor/24
-        resultado.config(text=f"{valor} horas = {resultado} días")
+        resultado.config(text=f"{valor} horas = {resultadoo} días")
 
-    def formulario(titulo, opciones, conversion):
-        ventana = tk.Toplevel()
-        ventana.title(titulo)
-        ventana.geometry("400x400")
+def formulario(titulo, opciones, conversion):
+    ventana = tk.Toplevel()
+    ventana.title(titulo)
+    ventana.geometry("200x200")
 
-        tk.Label(ventana, text="Valor a convertir:").pack(pady=5)
-        entrada = tk.Entry(ventana)
-        entrada.pack(pady=5)
+    tk.Label(ventana, text="Valor a convertir:").pack(pady=5)
+    entrada = tk.Entry(ventana)
+    entrada.pack(pady=5)
 
-        for nombre, tipo in opciones:
-            frame =tk.Frame(ventana)
-            frame.pack(pady=2)
-            boton = tk.Button(frame, text=nombre, command=lambda t=tipo: conversion(t, entrada, resultado))
-            boton.pack()
+    for nombre, tipo in opciones:
+        frame =tk.Frame(ventana)
+        frame.pack(pady=2)
+        boton = tk.Button(frame, text=nombre, command=lambda t=tipo: conversion(t, entrada, resultado))
+        boton.pack()
 
-        resultado = tk.Label(ventana, text="", fg= "pink")
+        resultado = tk.Label(ventana, text="", fg= "black")
         resultado.pack(pady=10)
 
-    principal = tk.Tk()
-    principal.title("Menú de Conversiones")
-    principal.geometry("400x400")
+principal = tk.Tk()
+principal.title("Menú de Conversiones")
+principal.geometry("200x200")
 
-    menu = tk.Menu(principal)
-    principal.config(menu=menu)
+menu = tk.Menu(principal)
+principal.config(menu=menu)
 
-    menu_conversion = tk.Menu(menu, tearoff=0)
-    menu.add_cascade(label="Conversión", menu=menu_conversion)
+menu_conversion = tk.Menu(menu, tearoff=0)
+menu.add_cascade(label="Conversiónes", menu=menu_conversion)
 
-    menu_conversion.add_command(label="Longitud", command=lambda: formulario(
-        "Conversión de longitudes",
-        [("Metros a kilometros", "metros_kilometros"), ("Pulgadas a Metros", "pulgadas_metros")],
-        longitudes))
+menu_conversion.add_command(label="Longitud", command=lambda: formulario(
+    "Conversión de longitudes",
+    [("Metros a kilometros", "metros_kilometro"), ("Pulgadas a Metros", "pulgadas_metros")],
+    longitudes))
     
-    menu_conversion.add_command(label="Masa", command=lambda: formulario(
-        "Conversión de Masa",
-        [("Kilogramos a gramos", "kilogramo_gramo"), ("Libras a kilogramos", "libra_kilogramo")],
-        masa))
+menu_conversion.add_command(label="Masa", command=lambda: formulario(
+    "Conversión de Masa",
+    [("Kilogramos a gramos", "kilogramo_gramo"), ("Libras a kilogramos", "libra_kilogramo")],
+    masa))
     
-    menu_conversion.add_command(label="Tiempo", command=lambda: formulario(
-        "Conversión de tiempo",
-        [("Segundos a minutos", "segundo_minuto"), ("Horas a días", "hora_dia")],
-        tiempo))
+menu_conversion.add_command(label="Tiempo", command=lambda: formulario(
+    "Conversión de tiempo",
+    [("Segundos a minutos", "segundo_minuto"), ("Horas a días", "hora_dia")],
+    tiempo))
     
-    principal.mainloop()
+principal.mainloop()
 
 
