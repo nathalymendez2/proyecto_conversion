0133d05ac35854cced23295f8afce36bf108d020
nathalymendez2 <20240188@colegioscj.edu.gt>
2025-05-15 22:56:47 -0600

    actualizacion 6

--- hola.py
+++ hola.py
@@ -32,5 +32,19 @@ def masa(tipo, entrada, resultado):
     
     elif tipo == "libra_kilogramo":
         resultadoo = valor*0.4
-        resultado-config(text=f"{valor} lb = {resultado} kg")
+        resultado.config(text=f"{valor} lb = {resultado} kg")
 
+def tiempo(tipo, entrada, resultado):
+    valor = validar(entrada.get())
+    if valor is None:
+        return
+    
+    if tipo == "segundo_minuto":
+        resultadoo = valor/60
+        resultado.config(text=f"{valor} segundos = {resultado} minutos")
+
+    elif tipo == "hora_dia":
+        resultadoo = valor/24
+        resultado.config(text=f"{valor} horas = {resultado} días")
+
+    
