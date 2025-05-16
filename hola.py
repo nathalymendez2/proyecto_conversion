5cf54448504bd938489f122fe88f24bff9f05eef
nathalymendez2 <20240188@colegioscj.edu.gt>
2025-05-15 22:18:01 -0600

    actualizacion 4

--- hola.py
+++ hola.py
@@ -1,3 +1,10 @@
-print("hola mundo")
-print("holaa")
-print("no")
+import tkinter as tk
+from tkinter import messagebox
+
+def validar(valor):
+    try:
+        return float(valor)
+    except ValueError:
+        messagebox.showerror("Porfavor, ingrese otro número.")
+        return None
+    
