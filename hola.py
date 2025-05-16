314128a033aaa13408761f8f8049cea58a4ea3d4
nathalymendez2 <20240188@colegioscj.edu.gt>
2025-05-15 22:51:21 -0600

    actualizacion 5

--- hola.py
+++ hola.py
@@ -8,3 +8,29 @@ def validar(valor):
         messagebox.showerror("Porfavor, ingrese otro número.")
         return None
     
+def longitudes(tipo, entrada, resultado):
+    valor = validar(entrada.get())
+    if valor is None:
+        return
+    
+    if tipo == "metro_kilometro":
+        resultadoo = valor /1000
+        resultado.config(text=f"{valor} pulgadas = {resultado} km")
+    
+    elif tipo == "pulgada_metro":
+        resultadoo = valor * 0.02
+        resultado.config(text=f"{valor} pulgadas = {resultado} metros")
+
+def masa(tipo, entrada, resultado):
+    valor = validar(entrada.get())
+    if valor is None:
+        return
+    
+    if tipo == "kilogramo_gramo":
+        resultadoo = valor*1000
+        resultado.config(text=f"{valor} kg = {resultado} g")
+    
+    elif tipo == "libra_kilogramo":
+        resultadoo = valor*0.4
+        resultado-config(text=f"{valor} lb = {resultado} kg")
+
